8253e21f1389478277e7d3e37bec727564632919
paulo626 <117474750+paulo626@users.noreply.github.com>
2024-06-18 13:34:23 -0300

    Add files via upload
    Codigo js finalizado(99%) presisa desbugar

--- script.js
+++ script.js
@@ -1,22 +1,124 @@
-function pegarvalor(){
-    var usuario = document.getElementById("inputusuario").value
-    var senha = document.getElementById("inputsenha").value
-    return{Usuario:usuario,Senha:senha}
-}
 
-function validar() {
-    var listaDeUsuarios = ["usuario1","usuario2","usuario3","Usuario1","Usuario2","Usuario3"]
-    var listaDeSenhas = ["senha1","senha2","senha3","Senha1","Senha2","Senha3"]
-    var valoresPegados= pegarvalor()
-    if(listaDeUsuarios.includes(valoresPegados.Usuario)){
-        if(listaDeSenhas.includes(valoresPegados.Senha)){
-            window.location.href ="http://127.0.0.1:5500/segundapagina.html"
-        }
+var senhas=['pokemon']
+var usuarios=['pokemon']
+
+function ValidarItem(item){
+    if(item == null || item == undefined){
+        return(false)
+    }
+    else if(isNaN(item)){
+        return(true)
+    }
+    else{return(false)}}
+
+function ValidarItemN(itemN){
+    if(itemN ==null|| itemN==undefined){
+        return(false)
     }
     else{
-        alert("sai dai vagabundo")
-        window.location.href ="https://www.youtube.com/index"
+        return(true)
     }
+}
+
+function Nconta(){
+    Validacao = false
+   do{
+        var V0 = false;
+        do{ 
+           var nome = prompt('digite se nome')
+           var chamado0 = ValidarItem(nome)
+           if(chamado0 == false){alert('nome invalido')}
+           else{
+                if(nome.length<2){alert('nome invalido')}
+                else{V0 = true}
+            }
+        }while(V0 === false);
+
+        var V1 =false;
+        do{
+            var email = prompt('digite se email')
+            var chamado1 = ValidarItem(email)
+            if (chamado1 == false){alert('email invalido')}
+            else{V1 = true}
+        }
+        while(V1 === false)
+
+
+        var V2 = false;
+        do{
+            var idade = parseInt(prompt('digite sua idade'))
+            var chamado2 = ValidarItemN(idade)
+            if(chamado2 == false){alert('idade invalida')}
+            else{
+                if(idade<0 || idade>100){alert('idade invalida')}
+                else{V2 = true}
+            }
+        }while(V2 === false)
+
+
+        var V3 =false;
+        do{
+            var senha = prompt('digite uma senha(DEVE CONTER LETRAS E CARACTERES MINIMO 6 CARACTERES)')
+            var chamado3 = ValidarItem(senha)
+            if(chamado3 == false){alert('senha invalida');}
+            else{
+                if(senha.length<6){alert('senha muito curta');}
+                else{V3 = true};
+            }
+        }while(V3 === false);
+
+
+        var V4 = false;
+        do{
+            var senhaConfirmada = prompt('confirme a senha');
+            var chamado4 = ValidarItem(senhaConfirmada);
+            if(chamado4 == false){alert('senha invalida')}
+            else{if(senhaConfirmada=!senha){alert('senha diferentes')}else{V4=true}}
+
+        }while(V4 === false);
+
+
+        var V5=false;
+        do{
+            var genero = prompt('digite seu genero (M ou F)');
+            var chamado5 =ValidarItem(genero);
+            var generoValidados = ['f','F','M','m']
+            if(chamado5 == false){alert('genero invalido')}
+            else{
+                if(generoValidados.includes(genero)){V5 = true;Validacao = true}
+                else(alert('genero invalido'))
+            }
+            
+
+        }while(V5 === false)
+}while(Validacao === false)
+
+senhas.push(senha);
+usuarios.push(nome);
 
 }
 
+function Validar(){
+    let Login = document.getElementById('Login').value;
+    let Senha = document.getElementById('Senha').value;
+    const Alertar = document.getElementById('Alertar')
+
+    if(senhas.includes(Login) && usuarios.includes(Senha)){
+        alert("login efetuado com sucesso")
+        window.location.href = 'pagina2.html';
+    }
+    else{
+        Alertar.style.color = 'red'
+        Alertar.innerText = 'usuario ou senha invalido'
+        console.log(Login)
+        console.log(Senha)
+    }
+}
+
+var lista = ['pokemon']
+var lista2 = ['pokemon']
+let elemento = "esquardou"
+let elemento2 = 'quadrobranco'
+lista.push(elemento)
+lista.push(elemento2)
+if(lista.includes(elemento)){console.log('deu bom')}else{console.log('tem coisa errada')}
